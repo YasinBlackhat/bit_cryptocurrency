e0a4d2ba5aafc1fe0f80209f79f8bbc879ea461e
yasin <58345572+YasinBlackhat@users.noreply.github.com>
2020-07-21 08:58:24 +0430

    Update charts.py

--- bit_cryptocurrency-V0.1/module/charts.py
+++ bit_cryptocurrency-V0.1/module/charts.py
@@ -6,22 +6,27 @@ import seaborn as sns
 import requests
 import os
 sns.set()
+# import library and set seaborn library
 
-def _start_init():
+def _start_init(): 
     
     try:
         res = requests.get('https://arzdigital.com/coins/')
         sub = BeautifulSoup(res.text, 'html.parser')
+        # check and request to web
+        # web Crawling
     except:
         return "check".lower()
 
     mk1 = 'app_BTC'
     f3 = os.path.exists('app_BTC')
+    # check file
     
     if f3 == False :
         mk = os.mkdir(mk1)
+        # creat folder
         
-    if res.status_code == 200:
+    if res.status_code == 200: 
         print('\n'+Fore.MAGENTA+' Bitcoin weekly chart this week \n')
         ur = sub.find('img',attrs={'alt':'bitcoin-chart'})
         url = ur['data-src']
